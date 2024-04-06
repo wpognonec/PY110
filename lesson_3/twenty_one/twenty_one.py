1a87f565ba33e85d74cf820bbcc31bd26fdfe369
William Pognonec <william.pognonec@gmail.com>
2024-04-05 22:26:06 -0500

    Dealing cards

--- lesson_3/twenty_one/twenty_one.py
+++ lesson_3/twenty_one/twenty_one.py
@@ -3,22 +3,8 @@ from random import choice, shuffle
 
 SUITS = ["♠", "♥", "♦", "♣"]
 
-VALUES = {
-    "1": 1,
-    "2": 2,
-    "3": 3,
-    "4": 4,
-    "5": 5,
-    "6": 6,
-    "7": 7,
-    "8": 8,
-    "9": 9,
-    "10": 10,
-    "J": 10,
-    "Q": 10,
-    "K": 10,
-    "A": [1, 11]
-}
+VALUES = ["1", "2", "3", "4", "5", "6", "7", 
+          "8", "9", "10", "J", "Q", "K", "A"]
 
 def display_cards(cards: list):
     for i in range(7):
@@ -36,15 +22,43 @@ def create_card_ascii(card: list):
             "└─────────┘"]
 
 def generate_deck():
-    deck = []
-    for suit in SUITS:
-        for value in VALUES:
-            deck.append([suit, value])
+    deck = [[suit, value] for value in VALUES for suit in SUITS]
     shuffle(deck)
     return deck
 
-deck = generate_deck()
+def total_cards(cards: list):
+    total = 0
+    aces = 0
+    for card in cards:
+        if card[1] in ["J", "Q", "K", "10"]:
+            total += 10
+        elif card[1] in "123456789":
+            total += int(card[1])
+        elif card[1] == "A":
+            aces += 1
+    for _ in range(aces):
+        if total < 12:
+            total += 11
+        else:
+            total += 1
+    return total
 
-card1 = create_card_ascii(deck.pop())
-card2 = create_card_ascii(deck.pop())
+def deal_cards(deck: list, num: int):
+    cards = []
+    for _ in range(num):
+        cards.append(deck.pop())
+    return cards
+
+deck = generate_deck()
+print(len(deck))
+hand = deal_cards(deck, 2)
+total = total_cards(hand)
+hand.extend(deal_cards(deck, 1))
+total = total_cards(hand)
+print(hand)
+print(total)
+
+card1 = create_card_ascii(hand[0])
+card2 = create_card_ascii(hand[1])
 display_cards([card1, card2])
+print(len(deck))
