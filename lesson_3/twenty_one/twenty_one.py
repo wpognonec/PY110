6341da2d7524ae29ab52fb5082b2f1f543f1f6ed
William Pognonec <william.pognonec@gmail.com>
2024-04-10 15:49:02 -0500

    Renamed and reorganized some functions

--- lesson_3/twenty_one/twenty_one.py
+++ lesson_3/twenty_one/twenty_one.py
@@ -9,18 +9,6 @@ VALUES = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 def msg(message: str):
     print("==>", message)
 
-def display_hand(cards: list, hidden=False):
-    ascii_cards = []
-    for card in cards:
-        if card == cards[-1] and hidden:
-            ascii_cards.append(create_card_ascii(card, hidden=True))
-        else:
-            ascii_cards.append(create_card_ascii(card))
-
-    for i in range(7):
-        ascii_line = " ".join(card[i] for card in ascii_cards)
-        print(ascii_line)
-
 def create_card_ascii(card: list, hidden=False):
     if hidden:
         return ["┌─────────┐", "│ ?       │",
@@ -37,12 +25,60 @@ def create_card_ascii(card: list, hidden=False):
             "│         │", f"│       {card[1]} │",
             "└─────────┘"]
 
-def generate_deck():
+def display_hand(cards: list, hidden=False):
+    ascii_cards = []
+    for card in cards:
+        if card == cards[-1] and hidden:
+            ascii_cards.append(create_card_ascii(card, hidden=True))
+        else:
+            ascii_cards.append(create_card_ascii(card))
+
+    for i in range(7):
+        ascii_line = " ".join(card[i] for card in ascii_cards)
+        print(ascii_line)
+
+def display_cards(player, dealer, hidden=False):
+    os.system(CLEAR)
+    msg("Dealer Cards:")
+    display_hand(dealer, hidden=hidden)
+    msg("Player Cards:")
+    display_hand(player)
+    if hidden:
+        display_totals(player, dealer[:-1])
+    else:
+        display_totals(player, dealer)
+
+def display_totals(player, dealer):
+    player_total = get_hand_total(player)
+    dealer_total = get_hand_total(dealer)
+    msg(f"The dealer has {dealer_total}")
+    msg(f"You have {player_total}")
+
+def display_winner(player, dealer):
+    dealer_total = get_hand_total(dealer["hand"])
+    player_total = get_hand_total(player["hand"])
+
+    if dealer_total > 21:
+        msg("Dealer busted! You Win!")
+        player["money"] += player["bet"] * 1.5
+    elif player_total > 21:
+        msg("You busted! Dealer Wins!")
+        player["money"] -= player["bet"]
+    elif dealer_total == player_total:
+        msg("This round is a tie!")
+    elif dealer_total > player_total:
+        msg("Dealer Wins!")
+        player["money"] -= player["bet"]
+    else:
+        msg("You Win!")
+        player["money"] += player["bet"] * 1.5
+
+def init_deck():
     deck = [[suit, value] for value in VALUES for suit in SUITS]
     shuffle(deck)
     return deck
 
-def total_cards(cards: list):
+def get_hand_total(cards: list):
     total = 0
     aces = 0
     for card in cards:
@@ -65,7 +101,7 @@ def deal_cards(deck: list, num: int):
         cards.append(deck.pop())
     return cards
 
-def hit_or_stay():
+def input_hit_or_stay():
     while True:
         choice = input("==> Would you like to hit or stay? ").strip().lower()
         if choice in ["hit", "h"]:
@@ -74,48 +110,12 @@ def hit_or_stay():
             return False
         msg("Incorrect input")
 
-def display_totals(player, dealer):
-    player_total = total_cards(player)
-    dealer_total = total_cards(dealer)
-    msg(f"The dealer has {dealer_total}")
-    msg(f"You have {player_total}")
-
-def display_cards(player, dealer, hidden=False):
-    os.system(CLEAR)
-    msg("Dealer Cards:")
-    display_hand(dealer, hidden=hidden)
-    msg("Player Cards:")
-    display_hand(player)
-    if hidden:
-        display_totals(player, dealer[:-1])
-    else:
-        display_totals(player, dealer)
-
-def display_winner(player, dealer):
-    dealer_total = total_cards(dealer["hand"])
-    player_total = total_cards(player["hand"])
-
-    if dealer_total > 21:
-        msg("Dealer busted! You Win!")
-        player["money"] += player["bet"] * 1.5
-    elif player_total > 21:
-        msg("You busted! Dealer Wins!")
-        player["money"] -= player["bet"]
-    elif dealer_total == player_total:
-        msg("This round is a tie!")
-    elif dealer_total > player_total:
-        msg("Dealer Wins!")
-        player["money"] -= player["bet"]
-    else:
-        msg("You Win!")
-        player["money"] += player["bet"] * 1.5
-
-def play_again(game_number):
+def input_play_again(game_number):
     if game_number:
         return input("==> Would you like to play again? (y / n)") == "y"
     return True
 
-def get_bet(money: int):
+def input_bet(money: int):
     while True:
         bet = int(input(
             f"==> You have {money}$. How much would you like to bet? "
@@ -124,13 +124,13 @@ def get_bet(money: int):
             return bet
         msg("You do not have enough for that bet.")
 
-def player_turn(player):
-    player_total = total_cards(player["hand"])
-    if player_total > 20 or not hit_or_stay():
+def is_player_turn(player):
+    player_total = get_hand_total(player["hand"])
+    if player_total > 20 or not input_hit_or_stay():
         return False
     return True
 
-def game():
+def main():
     game_number = 0
     player = {
         "hand": [],
@@ -138,22 +138,22 @@ def game():
         "bet": 0
     }
     dealer = {"hand": []}
-    while play_again(game_number):
-        deck = generate_deck()
-        player["bet"] = get_bet(player["money"])
+    while input_play_again(game_number):
+        deck = init_deck()
+        player["bet"] = input_bet(player["money"])
         player["hand"] = deal_cards(deck, 2)
         dealer["hand"] = deal_cards(deck, 2)
         display_cards(player["hand"], dealer["hand"], hidden=True)
 
-        while player_turn(player):
+        while is_player_turn(player):
             player["hand"].extend(deal_cards(deck, 1))
             display_cards(player["hand"], dealer["hand"], hidden=True)
 
-        if total_cards(player["hand"]) < 21:
+        if get_hand_total(player["hand"]) < 21:
             time.sleep(1)
             display_cards(player["hand"], dealer["hand"])
 
-            while total_cards(dealer["hand"]) < 17:
+            while get_hand_total(dealer["hand"]) < 17:
                 time.sleep(1)
                 dealer["hand"].extend(deal_cards(deck, 1))
                 display_cards(player["hand"], dealer["hand"])
@@ -161,4 +161,4 @@ def game():
         display_winner(player, dealer)
         game_number += 1
 
-game()
+main()
