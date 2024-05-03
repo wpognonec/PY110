0f558268c15f7d9981893453bf6067627385e2a1
William Pognonec <william.pognonec@gmail.com>
2024-05-03 14:28:49 -0500

    Egyptian fractions

---
+++ small_problems/advanced/ex_7.py
@@ -0,0 +1,33 @@
+from fractions import Fraction
+
+def egyptian(frac: Fraction):
+    curr_den = 1
+    dens = []
+    while frac:
+        unit_frac = Fraction(1,curr_den)
+        if unit_frac <= frac:
+            dens.append(curr_den)
+            frac -= unit_frac
+        curr_den += 1
+    return dens
+
+def unegyptian(lst: list):
+    return sum(Fraction(1, d) for d in lst)
+
+# Using the egyptian function
+# Your results may differ for these first 3 examples
+print(egyptian(Fraction(2, 1)))      # [1, 2, 3, 6]
+print(egyptian(Fraction(137, 60)))   # [1, 2, 3, 4, 5]
+print(egyptian(Fraction(3, 1)))
+# [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 230, 57960]
+
+# Using the unegyptian function
+# All of these examples should print True
+print(unegyptian(egyptian(Fraction(1, 2))) == Fraction(1, 2))
+print(unegyptian(egyptian(Fraction(3, 4))) == Fraction(3, 4))
+print(unegyptian(egyptian(Fraction(39, 20))) == Fraction(39, 20))
+print(unegyptian(egyptian(Fraction(127, 130))) == Fraction(127, 130))
+print(unegyptian(egyptian(Fraction(5, 7))) == Fraction(5, 7))
+print(unegyptian(egyptian(Fraction(1, 1))) == Fraction(1, 1))
+print(unegyptian(egyptian(Fraction(2, 1))) == Fraction(2, 1))
+print(unegyptian(egyptian(Fraction(3, 1))) == Fraction(3, 1))
